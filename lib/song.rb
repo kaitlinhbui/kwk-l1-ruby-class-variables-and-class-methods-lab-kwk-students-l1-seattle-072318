c388ad2d9467241de1cc20bceef848443794b35e
kaitlinhbui <kaitlinhb01@gmail.com>
2018-07-30 16:17:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,15 @@
 class Song
   attr_accessor(name, artist, genre)
+  
+  
+  
   @@count = 0
   def self.count
     @@count
   end
+  
   def initialize(name, artist, genre)
-    
+    @@count += 1
   end
 end
 
